80353b44dd6d77be3702bb9ded311852bcfd90e4
niketmaurya <157571238+niketmaurya@users.noreply.github.com>
2026-05-07 16:48:57 +0530

    Add JWT middleware and bookmark functionality

--- backend/controllers/storyController.js
+++ backend/controllers/storyController.js
@@ -1,4 +1,5 @@
 const Story = require("../models/Story");
+const User = require("../models/User");
 
 const getStories = async (req, res) => {
   try {
@@ -30,7 +31,48 @@ const getSingleStory = async (req, res) => {
   }
 };
 
+
+const toggleBookmark = async (req, res) => {
+
+    try {
+
+        const storyId = req.params.id;
+
+        const user = await User.findById(req.user._id);
+
+        const alreadyBookmarked =
+            user.bookmarks.includes(storyId);
+
+        if (alreadyBookmarked) {
+
+            user.bookmarks = user.bookmarks.filter(
+                (id) => id.toString() !== storyId
+            );
+
+        } else {
+
+            user.bookmarks.push(storyId);
+        }
+
+        await user.save();
+
+        res.status(200).json({
+            message: alreadyBookmarked
+                ? "Bookmark removed"
+                : "Bookmark added",
+            bookmarks: user.bookmarks,
+        });
+
+    } catch (error) {
+
+        res.status(500).json({
+            message: error.message,
+        });
+    }
+};
+
 module.exports = {
   getStories,
   getSingleStory,
+  toggleBookmark,
 };
--- backend/routes/storyRoutes.js
+++ backend/routes/storyRoutes.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const { protect } = require("../middleware/authMiddleware");
 
 const {
   getStories,
   getSingleStory,
+  toggleBookmark,
 } = require("../controllers/storyController");
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.get("/", getStories);
 
 router.get("/:id", getSingleStory);
+
+router.post("/:id/bookmark", protect, toggleBookmark);
 
 module.exports = router;
